2d77bd17d70a704bc56df49ecb38eafa5678d2f3
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-22 23:50:34 -0500

    commit tree 329d52324145cad64fd212429ed5daeee76ded7e

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+// function submitData(name, email){
+    
+//     const userData = {name, email}
+//     const configObject = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type" : "application/json",
+//             "Accepts" : "application/json"
+//         },
+//         body: JSON.stringify(userData)
+//     }
+    
+//     return fetch( 'https://localhost:3000/users', configObject)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(resp){
+//         document.body.innerHTML = resp["id"]
+//     })
+//     .catch(function(error){
+//         document.body.innerHTML = error.message
+//     })
+// }
+// const userData = {}
+
+// const bodyElement = document.querySelector("body")
+
+// .then(function(response){
+//     return response.json()
+// })
+// .then(function(resp){
+//     document.body.innerHTML = resp["id"]
+// })
+// .catch(function(error){
+//     document.body.innerHTML = error.message
+// })
+
+function submitData( name, email ) {
+    const userData = {name: `${name}`, email: `${email}`}
+    const configObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+        } 
+        
+    return fetch( 'http://localhost:3000/users', configObject )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( resp ) {
+        document.body.innerHTML = resp[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
